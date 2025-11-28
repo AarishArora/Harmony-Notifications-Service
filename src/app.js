530f2ea3332014584e56d8e18d19dac0e4d3f978
Aarish Kumar <aarisharora777@gmail.com>
2025-11-28 11:27:52 +0530

    added health route

--- src/app.js
+++ src/app.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+
+app.get('/health', (req, res) => {
+	return res.status(200).json({ service: "notifications", status: "healthy" });
+});
+
 export default app;
